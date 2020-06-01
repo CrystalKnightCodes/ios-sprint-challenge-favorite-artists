9c9bb73d92a4c446c3a28071bca0616f520a408b
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-31 22:31:22 -0400

    Created artist model

---
+++ Favorite Artists/Favorite Artists/Artist.m
@@ -0,0 +1,36 @@
+//
+//  Artist.m
+//  Favorite Artists
+//
+//  Created by Christy Hicks on 5/31/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+- (instancetype)initWithArtist:(NSString *)artist yearFormed:(int)yearFormed biography:(NSString *)biography {
+    self = [super init];
+    
+    if (self) {
+        _artist = [artist copy];
+        _yearFormed = yearFormed;
+        _biography = [biography copy];
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    NSString *artist = dictionary[@"strArtist"];
+    NSNumber *NSyearFormed = dictionary[@"intFormedYear"];
+    NSString *biography = dictionary[@"strBiographyEN"];
+    
+    if ([NSyearFormed isKindOfClass:[NSNull class]]) return nil;
+    
+    int yearFormed = NSyearFormed.intValue;
+    
+    return [self initWithArtist:artist yearFormed:yearFormed biography:biography];
+}
+
+@end
