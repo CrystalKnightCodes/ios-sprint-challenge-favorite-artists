d0456ac43d4941abd796bbc868d575216b857132
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-31 21:47:43 -0400

    Initial Commit

---
+++ Favorite Artists/Favorite Artists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Favorite Artists
+//
+//  Created by Christy Hicks on 5/31/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Favorite Artists/Favorite Artists/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Favorite Artists
+//
+//  Created by Christy Hicks on 5/31/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
